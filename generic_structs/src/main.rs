930e6697537f697064433ed5e0dc7b1fd5863814
alfonmga <alfonmga@icloud.com>
2022-03-07 19:34:01 +0100

    Update

---
+++ generic_structs/src/main.rs
@@ -0,0 +1,22 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+impl Point<f32, f32> {
+    fn distance_from_origen(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 11, y: 4.0 };
+
+    println!("float.x = {}", float.x());
+}
